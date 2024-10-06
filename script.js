1fd1166d514003cf2bb3e57fc2b6b2eecc5b05c4
Evgueni Kounik <kounik.ch@gmail.com>
2024-10-06 04:08:45 +0200

    Testing and refactoring for mobile

--- script.js
+++ script.js
@@ -6,27 +6,6 @@ const jokeInput = document.getElementById("jokeInput");
 const speakButton = document.getElementById("speakButton");
 
 let voices = [];
-let currentLanguage;
-
-function getVoices() {
-  return new Promise((resolve, reject) => {
-    // Check if voices are already loaded
-    voices = window.speechSynthesis.getVoices();
-    if (voices.length > 0) {
-      resolve(voices);
-    } else {
-      // Wait for the event if voices are not loaded yet
-      window.speechSynthesis.onvoiceschanged = () => {
-        voices = window.speechSynthesis.getVoices();
-        if (voices.length > 0) {
-          resolve(voices); // Resolve promise with loaded voices
-        } else {
-          reject('No voices available');
-        }
-      };
-    }
-  });
-}
 
 async function getJokes(language = 'en') {
   let joke;
@@ -64,105 +43,16 @@ function populateLanguages() {
 }
 
 async function populateVoices() {
-  voices = await getVoices();
   if (languageSelect.children.length === 0) {
     populateLanguages();
   }
-  currentLanguage = languageSelect.value;
-  console.log(currentLanguage);
-  // console.log(voices);
-
-
-
-  // const language = languageSelect.value || 'en';
-  // voices = synth.getVoices().filter(function (voice) { return voice.lang.includes(language); }).sort(function (a, b) {
-  //   const aname = a.name.toUpperCase();
-  //   const bname = b.name.toUpperCase();
-
-  //   if (aname < bname) {
-  //     return -1;
-  //   } else if (aname == bname) {
-  //     return 0;
-  //   } else {
-  //     return +1;
-  //   }
-  // });
-  // const selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-  // voiceSelect.innerHTML = "";
-
-  // for (let i = 0; i < voices.length; i++) {
-  //   const option = document.createElement("option");
-  //   option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
-  //   if (voices[i].default) {
-  //     option.textContent += " -- DEFAULT";
-  //   }
-
-  //   option.setAttribute("data-lang", voices[i].lang);
-  //   option.setAttribute("data-name", voices[i].name);
-  //   voiceSelect.appendChild(option);
-  // }
-  // voiceSelect.selectedIndex = selectedIndex;
-
-}
-
-// async function loadVoiceList() {
-//   if (languageSelect.children.length === 0) {
-//     populateLanguages();  
-//   }
-//   voices = await loadVoices();
-//   // populateVoiceList();
-//   // if (speechSynthesis.onvoiceschanged !== undefined) {
-//   //   speechSynthesis.onvoiceschanged = populateVoiceList;
-//   // }
-//   console.log('language', languageSelect.value);
-//   console.log(voices);
-// }
-
-// get joke from API
-
-
-async function tellJoke() {
-  // const selectedIndex = voiceSelect.selectedIndex;
-  // const selectedOption = voiceSelect.options[selectedIndex]
-  // const selectedLang = selectedOption.getAttribute('data-lang');
-  // const selectedVoices = selectedOption.getAttribute('data-name');
-  // console.log(selectedIndex);
-  // console.log(selectedOption);
-  // console.log(selectedLang);
-  // console.log(selectedVoices);
-
-  // const voiceName = voiceSelect.options[voiceSelect.selectedIndex].getAttribute('data-name');
-
-  const joke = await getJokes(languageSelect.value);
-
-  const utterThis = new SpeechSynthesisUtterance(joke);
-
-  utterThis.onend = function (event) {
-    console.log("SpeechSynthesisUtterance.onend");
-  };
-
-  utterThis.onerror = function (event) {
-    console.error("SpeechSynthesisUtterance.onerror");
-  };
-
-  const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
-
-  for (let i = 0; i < voices.length; i++) {
-    if (voices[i].name === selectedOption) {
-      utterThis.voice = voices[i];
-      break;
-    }
-  }
-  
-  synth.speak(utterThis);
+  console.log('language:', languageSelect.value);
 }
 
 async function speak() {
   // const joke = 'Warum sollte man nie Cola und Bier gleichzeitig trinken? ... Weil man dann colabiert.';
-  const joke = await getJokes(currentLanguage);
+  const joke = await getJokes(languageSelect.value);
   console.log(joke);
-
   const utterThis = new SpeechSynthesisUtterance(joke);
   synth.speak(utterThis);
 }
